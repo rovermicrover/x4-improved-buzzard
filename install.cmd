7414a5bf10842cf54050e1efe574d6af6484a45a
Andrew Rove <rovermicrover@gmail.com>
2019-10-25 00:29:25 -0500

    Fix build tools and add logos.

--- install.cmd
+++ install.cmd
@@ -2,5 +2,3 @@ del /F /S /Q "C:\Program Files (x86)\Steam\steamapps\common\X4 Foundations\exten
 mkdir "C:\Program Files (x86)\Steam\steamapps\common\X4 Foundations\extensions\improvedbuzzard"
 xcopy .\content.xml "C:\Program Files (x86)\Steam\steamapps\common\X4 Foundations\extensions\improvedbuzzard\"
 xcopy .\assets "C:\Program Files (x86)\Steam\steamapps\common\X4 Foundations\extensions\improvedbuzzard\assets\" /E
-xcopy .\libraries "C:\Program Files (x86)\Steam\steamapps\common\X4 Foundations\extensions\improvedbuzzard\libraries\" /E
-xcopy .\md "C:\Program Files (x86)\Steam\steamapps\common\X4 Foundations\extensions\improvedbuzzard\md\" /E
